9466422a5576ef6952b088c5fc48476f0d3c2675
xueerchen1990 <xueer.chen.human@gmail.com>
2024-09-08 22:06:05 -0400

    streaming works

--- try_api.py
+++ try_api.py
@@ -8,8 +8,14 @@ def main():
     genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content("Write a story about a magic backpack.")
-    print(response.text)
+    # response = model.generate_content("Write a story about a magic backpack.")
+    # print(response.text)
+
+    # call gemini stream the response
+    for chunk in model.generate_content("Write a story about a magic backpack.", stream=True):
+        print(chunk.text)
+    
+    
 
 if __name__ == "__main__":
     main()
